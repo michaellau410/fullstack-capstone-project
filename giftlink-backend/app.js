03469fb4042b9fd3e554dcb8fea786f2265da10a
Michael Lau <michael.lau.410@gmail.com>
2026-09-11 07:05:00 -0400

    update

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,6 +7,7 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 const giftRoutes = require("./routes/giftRoutes");
+const searchRoutes = require("./routes/searchRoutes");
 
 const app = express();
 app.use("*",cors());
@@ -38,7 +39,7 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 //{{insert code here}}
-
+app.use('/api/search',searchRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
 
