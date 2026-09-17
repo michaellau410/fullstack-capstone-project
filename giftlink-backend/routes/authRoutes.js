91f11d7750814c3ccaed882221681c46da5605fc
michaellau410 <71592478+michaellau410@users.noreply.github.com>
2026-09-17 13:24:08 +0800

    Enable Pino logger and add login endpoint

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -23,9 +23,9 @@ const jwt = require('jsonwebtoken'); // woth bcrypt together for Login
 const { body, validationResult } = require('express-validator');
 
 // log
-//const pino = require('pino');
+const pino = require('pino');
 // Create a Pino logger instance, this must be outside any endpoint handling to avoid performance issue
-//const logger = pino();  
+const logger = pino();  
 //const logger = require('pino')(); // combined
 
 
@@ -52,14 +52,14 @@ router.post('/register', async (req, res) => {
 
             const payload = {
                 user: {
-                    id: newUser.insertedId,
+                    id: newUser.insertedId, //??
                 },
             };
 
             const authtoken = jwt.sign(payload, JWT_SECRET);
             return res.status(200).json({authtoken, email});
         } else {
-            return res.status(400).json({ message: "Email already exists" });
+            return res.status(400).json({ error: "Email already exists" });
         }
     } catch (e) {
         // ADD THIS LINE TO SEE THE ACTUAL ERROR IN YOUR TERMINAL
@@ -69,6 +69,65 @@ router.post('/register', async (req, res) => {
         return res.status(500).json({ error: e.message || 'Internal server error' });
     }
 });
+
+// login endpoint
+router.post('/login', async (req, res) =>{
+    try{
+        const db = await connectToDatabase();
+
+        // this collection line just set a reference but no loading anything
+        // so no need 'await'
+        const collection = db.collection("users");
+
+        const existingUser  = await collection.findOne({email: req.body.email});
+
+        if(existingUser)
+        {
+            //logger.error('User found');
+
+            const isPasswordMatched = await bcryptjs.compare(req.body.password, existingUser.password);
+            
+            
+            if(isPasswordMatched){
+
+                //logger.error('password ok');
+                //const payload = {email: req.body.email,};
+                // payload contains can be anything
+                // system only use the hashed token
+                let payload = {
+                    user: {
+                        id: existingUser._id.toString(),
+                    },
+                };
+
+
+                const authtoken = jwt.sign(payload, JWT_SECRET);
+                const userName = existingUser.firstName;
+                const userEmail = existingUser.email;
+
+
+                return res.status(200).json({authtoken, userName, userEmail });
+
+            }
+            else{
+                //logger.error('Wrong password');
+                return res.status(401).json({ error: "No such user or invalid password" });
+            }
+
+        }else{
+            //logger.error('User not found');
+            return res.status(401).json({ error: "No such user or invalid password" });
+        }
+
+
+    } catch (e)
+    {
+        //console.error("Full Error Details:", e); 
+
+        return res.status(500).send({ error: 'Internal server error'});
+    }
+});
+
 
 
 module.exports = router;
